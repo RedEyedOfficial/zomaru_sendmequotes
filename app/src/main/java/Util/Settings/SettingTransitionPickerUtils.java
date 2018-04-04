da5c2042a64fffbe578ab7bc71055a1b989107d6
zomaru <renaldypurwanto@gmail.com>
2018-04-04 08:33:22 +0700

    bersih-bersih

--- app/src/main/java/Util/Settings/SettingTransitionPickerUtils.java
+++ app/src/main/java/Util/Settings/SettingTransitionPickerUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018 Red Eyed Official
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package Util.Settings;
 
 import android.app.Activity;
@@ -12,10 +28,6 @@ import android.widget.Toast;
 
 import zomaru.sendmequotes.MainActivity;
 import zomaru.sendmequotes.R;
-
-/**
- * Created by root on 1/22/18.
- */
 
 public class SettingTransitionPickerUtils {
     private static int Wallpaper;
