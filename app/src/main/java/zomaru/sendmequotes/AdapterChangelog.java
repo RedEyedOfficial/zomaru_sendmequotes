4d68ff1b79e6c9f815ddd89c9fcadbcb868bb9e5
zomaru <renaldypurwanto@gmail.com>
2018-04-04 08:27:35 +0700

    bersih-bersih

--- app/src/main/java/zomaru/sendmequotes/AdapterChangelog.java
+++ app/src/main/java/zomaru/sendmequotes/AdapterChangelog.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018 Red Eyed Official
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package zomaru.sendmequotes;
 
 import android.support.v7.widget.RecyclerView;
@@ -9,10 +25,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
-/**
- * Created by root on 4/2/18.
- */
 
 public class AdapterChangelog extends RecyclerView.Adapter<AdapterChangelog.AdapterHolder> {
     private ArrayList<ChangelogInfo> arrayList;
